271fe6555c819b1272bd8bffe28ea0f8cdc3a2c0
Hui Jeong Lee <huijeong1015@gmail.com>
2023-09-19 09:59:12 -0400

    Fix typo inside example3.py

--- example3.py
+++ example3.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('example3-1.html')
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('example3-2.html', name=name)
+    return render_template('user.html', name=name)
 
