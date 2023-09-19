1575afa523e24d9fbdb39d0fe3592c533a9e9bd4
Hui Jeong Lee <huijeong1015@gmail.com>
2023-09-19 17:34:25 -0400

    Raise an error when the email field input is missing '@'

--- example4.py
+++ example4.py
@@ -5,18 +5,19 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
 
 app = Flask(__name__)
-app.debug = True
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 
-def check_uoft_email(form, field):
+def check_email(form, field):
+    if not '@' in field.data:
+        raise ValidationError('Please include an \'@\' in the email address. \'{:s}\' is missing an \'@\''.format(field.data))
     if not 'utoronto' in field.data:
         raise ValidationError('Enter U of T email address.')
 
 class Forms(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = StringField('What is your U of T email address?', validators=[DataRequired(), check_uoft_email])
+    email = StringField('What is your U of T email address?', validators=[DataRequired(), check_email])
     submit = SubmitField('Submit')
     
 @app.route('/', methods=['GET', 'POST'])
@@ -26,7 +27,6 @@ def index():
         old_name = session.get('name')
         if old_name is not None and old_name != forms.name.data:
             flash('Looks like you have changed your name!')
-            print("Flash message")
         session['name'] = forms.name.data
         session['email'] = forms.email.data
         return redirect(url_for('index'))
