4e987836583d05d7820840972613bc2aeba98b36
Hui Jeong Lee <huijeong1015@gmail.com>
2023-09-19 16:52:08 -0400

    Add a flask field for U of T email

--- example4.py
+++ example4.py
@@ -1,35 +1,36 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
-class NameForm(FlaskForm):
+def check_uoft_email(form, field):
+    if not 'utoronto' in field.data:
+        raise ValidationError('Enter U of T email address.')
+
+class Forms(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your U of T email address?', validators=[DataRequired(), check_uoft_email])
     submit = SubmitField('Submit')
-
+    
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
+    forms = Forms()
+    if forms.validate_on_submit():
         old_name = session.get('name')
-        print(old_name)
-        print(form.name.data)
-        if old_name is not None and old_name != form.name.data:
+        if old_name is not None and old_name != forms.name.data:
             flash('Looks like you have changed your name!')
             print("Flash message")
-        session['name'] = form.name.data
+        session['name'] = forms.name.data
+        session['email'] = forms.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', forms=forms, name=session.get('name'), email=session.get('email'))
 
 @app.errorhandler(404)
 def page_not_found(e):
